32541b6916c77d97c39a405f8895f368de338f0c
mrezagh96 <mrezagh1996@mail.com>
2022-12-15 15:48:38 +0330

    creating git

---
+++ go.mod
@@ -0,0 +1,3 @@
+module testgithub
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1
+	fmt.Println("this is a test for Github and git structures checking")
+	//2
+	fmt.Println("hi here is  mohammadreza")
+	// this is from sayhello branch
+	fmt.Println("say hello to everybody ")
+	// editing in other device
+	fmt.Println("this is an edit an other device ")
+	fmt.Println("OK")
+	fmt.Println("done")
+
+}
